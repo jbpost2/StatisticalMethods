50d28c60afe69763b450cf41a6113c642d7f6706
Matthew Avery <matthewravery@gmail.com>
2020-03-11 09:45:46 -0600

    Initialize counterfire radar dataset

---
+++ datasets/Experimental/counterfire-radar/generate-dataset.R
@@ -0,0 +1,62 @@
+# Counterfire radar data
+
+# Factors:
+# quadrant elevation
+# bore angle
+# radar mode
+# aspect angle
+# Projectile type
+# fire rate
+# shot range
+# weapon-to-radar range
+
+# response variables: P(Detect), Point of origin TLE
+
+# proportion histogram
+# two-sample proportion test
+# logistic regression
+# blocked/split plot design
+# binomial regression
+
+set.seed(20200311)
+library(tidyverse)
+library(skimr)
+
+n <- 2873
+nmission = 323
+missions <- base::sample(5:13, size = nmission, replace = T)
+
+tb <- tibble(
+  Mission = 1:nmission,
+  `Mission Size` = missions,
+  `Radar Mode` = base::sample(c("360 degree Normal", "90 degree Normal", "90 degree Short range"), 
+                        replace = T, size = nmission,
+                        prob = c(.4, .15, .45)),
+  `Shot Range` = rbeta(nmission, 2, 10) * 19000 + 600,
+  `Aspect Angle` = (rbeta(nmission, 2, 10)  + 
+                    rnorm(nmission, .5, .15) * rbinom(nmission, 1, .2)) * 170 + 5,
+  `Quadrant Elevation` = rbeta(nmission, 3, 2) * 1000 + 350,
+  `Radar-to-Weapon Range` = rbeta(nmission, 3, 2) * 25000 + 5000,
+  `Bore Angle` = rbeta(nmission, 1, 3) * 3500) 
+
+
+tb$Munition <- ifelse(tb$`Radar Mode` == "360 degree Normal",
+                      yes = sample(c("Artillery", "Mortar", "Rocket"), 
+                             size = nmission, replace = T, 
+                             prob = c(.6, .2, .2)),
+                      no = sample(c("Artillery", "Mortar", "Rocket"), 
+                             size = nmission, replace = T, 
+                             prob = c(.15, .7, .25)))
+
+
+
+tb$`Fire Rate` <- ifelse(Munition == "Rocket", 
+                         "Single Fire",
+                         sample(c("Single Fire", "Volley Fire"), size = nmission, replace = T,
+                                prob = c(.7, .3)))
+  
+tb %>% 
+  group_by(Munition) %>% 
+  skim
+
+tb <- write_csv("datasets/experimental/counterfire-radar/counterfire-radar.csv")
